801fe511c7180bd0474f9bc109bea0b9b13701a5
Temu-bit <timeosanta22@gmail.com>
2026-07-29 07:55:21 +0200

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Santa_Blanca_Kunstlabor/sw.js', { scope: '/Santa_Blanca_Kunstlabor/' })})}
